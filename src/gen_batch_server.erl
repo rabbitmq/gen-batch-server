a9a707f1c1d060b9c834b54fa1a1b6b98386199d
kjnilsson <knilsson@pivotal.io>
2019-08-29 11:58:08 +0100

    Restore OTP 21 compat
    Test both on OTP 21 and 22

--- src/gen_batch_server.erl
+++ src/gen_batch_server.erl
@@ -371,7 +371,8 @@ format_status(_Reason, [_PDict, SysState, Parent, Debug,
                                                 module = Mod},
                                state = State }]) ->
     Header = gen:format_status_header("Status for batching server", Name),
-    Log = sys:get_log(Debug),
+    Log = sys_get_log(Debug),
+
     [{header, Header},
      {data,
       [
@@ -385,6 +386,14 @@ format_status(_Reason, [_PDict, SysState, Parent, Debug,
              [State];
          T -> [T]
      end].
+
+-if(?OTP_RELEASE >= 22).
+sys_get_log(Debug) ->
+    sys:get_log(Debug).
+-else.
+sys_get_log(Debug) ->
+    sys:get_debug(log, Debug, []).
+-endif.
 
 write_debug(Dev, Event, Name) ->
     io:format(Dev, "~p event = ~p~n", [Name, Event]).
