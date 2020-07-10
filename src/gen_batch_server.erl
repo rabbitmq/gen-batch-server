b07c8b37c254554100cad95663450a50f47a3dc8
dcorbacho <dparracorbacho@piotal.io>
2020-07-10 15:38:52 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- src/gen_batch_server.erl
+++ src/gen_batch_server.erl
@@ -1,4 +1,9 @@
-%% Copyright (c) 2018-Present Pivotal Software, Inc. All Rights Reserved.
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2018-2020 VMware, Inc. or its affiliates.  All rights reserved.
+%%
 -module(gen_batch_server).
 -export([start_link/2,
          start_link/3,
